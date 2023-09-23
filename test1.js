40798c9f44c438204760dc0c6b8fc86593eb276c
tranquocthinh88 <97369919+tranquocthinh88@users.noreply.github.com>
2023-09-23 23:29:52 +0700

    commit tuan2

---
+++ test1.js
@@ -0,0 +1,14 @@
+const usrlBacon = "https://jsonplaceholder.typicode.com/todos/1";
+(function() {
+    $.ajax({
+            method: "GET",
+            url: usrlBacon,
+            dataType: "json"
+        })
+        .done(function(data) {
+            console.log(data);
+        })
+        .fail(function() {
+            alert("no good");
+        });
+})()
---
+++ test7.js
@@ -0,0 +1,14 @@
+// Delete a user
+// var url = "http://localhost:8080/api/v1/users";
+var url = "https://jsonplaceholder.typicode.com/todos/1";
+var xhr = new XMLHttpRequest();
+xhr.open("DELETE", url, true);
+xhr.onload = function() {
+    var users = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+        console.table(users);
+    } else {
+        console.error(users);
+    }
+};
+xhr.send(null);
